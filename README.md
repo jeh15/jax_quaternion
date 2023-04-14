7af413cfd979dcd886d0ef9756d662596ed3b5cd
jeh15 <jeh15@fsu.edu>
2023-04-14 00:14:29 -0400

    Added functions.py that contain various quaternion functions that are jax jit compatible as well as a unittest.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# jax_quaternion
-Jaxified version of rowan
+# Jaxified version of rowan
+Implementing the quaternion library [rowan](https://github.com/glotzerlab/rowan) with a JAX backend.
+
+This repo is a work in progress and will be updated when I need more features of rowan jaxified.
---
+++ unittest_functions.py
@@ -0,0 +1,127 @@
+from absl.testing import absltest
+from functools import partial
+
+import numpy as np
+import numpy.typing
+import jax
+import jax.numpy as jnp
+
+import rowan
+from rowan.functions import _promote_vec as rowan_promote_vec
+
+import functions
+
+
+class TestQuaternionMath(absltest.TestCase):
+    def test_exp(self):
+        # Random Matrix of Quaternions:
+        v = np.random.rand(10, 4)
+
+        # Exponential Quaternion Function:
+        result_1 = functions.exp(v)
+        result_2 = functions.exp(v[0, :])
+
+        # Rowan Result:
+        true_value = rowan.exp(v)
+
+        # Assertion Test:
+        np.testing.assert_allclose(
+            true_value, result_1,
+            atol=1e-6,
+        )
+        np.testing.assert_allclose(
+            true_value[0, :], result_2,
+            atol=1e-6,
+        )
+
+    def test_multiply(self):
+        qi = np.random.rand(10, 4)
+        qj = np.random.rand(10, 4)
+        result = functions.multiply(qi, qj)
+
+        # Rowan Result:
+        true_value = rowan.multiply(qi, qj)
+
+        # Assertion Test:
+        np.testing.assert_allclose(
+            true_value, result,
+            atol=1e-6,
+        )
+
+    def test_promote_vector(self):
+        v = np.random.rand(10, 3)
+        result = functions._jax_promote_vector(v)
+
+        # Rowan Result:
+        true_value = rowan_promote_vec(v)
+
+        # Assertion Test:
+        np.testing.assert_allclose(
+            true_value, result,
+            atol=1e-6,
+        )
+
+    def test_integrate(self):
+        q = np.random.rand(10, 4)
+        q = q / np.linalg.norm(q, axis=-1)[..., np.newaxis]
+        v = np.random.rand(10, 3)
+        dt = 0.1
+        result = functions.integrate(q, v, dt)
+
+        # Rowan Result:
+        true_value = rowan.calculus.integrate(q, v, dt)
+
+        # Assertion Test:
+        np.testing.assert_allclose(
+            true_value, result,
+            atol=1e-6,
+        )
+
+    def test_normalize(self):
+        q = np.random.rand(10, 4)
+        result_1 = functions.jax_normalize(q)
+        result_2 = functions.numpy_normalize(q)
+
+        # Rowan Result:
+        true_value = rowan.normalize(q)
+
+        # Assertion Test:
+        np.testing.assert_allclose(
+            true_value, result_1,
+            atol=1e-6,
+        )
+        np.testing.assert_allclose(
+            true_value, result_2,
+            atol=1e-6,
+        )
+
+    def test_conjugate(self):
+        q = np.random.rand(10, 4)
+        result = functions.conjugate(q)
+
+        # Rowan Result:
+        true_value = rowan.conjugate(q)
+
+        # Assertion Test:
+        np.testing.assert_allclose(
+            true_value, result,
+            atol=1e-6,
+        )
+
+    def test_rotation(self):
+        q = np.random.rand(10, 4)
+        v = np.random.rand(10, 3)
+        result = functions.rotate(q, v)
+
+        # Rowan Result:
+        true_value = rowan. rotate(q, v)
+
+        # Assertion Test:
+        np.testing.assert_allclose(
+            true_value, result,
+            atol=1e-6,
+        )
+
+
+if __name__ == "__main__":
+    absltest.main()
